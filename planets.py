cb513f983f93dc9b91dc73b764d354815710a02b
mmurvihi <mmurvihi@calpoly.edu>
2018-09-22 17:44:56 -0700

    planets.py

--- planets.py
+++ planets.py
@@ -1,5 +1,16 @@
+# 
+#Margot Murvihill
+#Professor Humer 
+#CPE 202
+#Lab9
+
 def weight_on_planets():
-   # write your code here
+
+   earth=float(input("What do you weigh on earth? "))
+   mars=0.38*earth
+   jupiter=2.34*earth
+   print("\nOn Mars you would weigh {} pounds.".format(mars) + "\nOn Jupiter you would weigh {} pounds.".format(jupiter))
+
    
    
    
